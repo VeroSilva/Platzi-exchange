54a20535f4bedf9ecb45fadb6c59339c19c43151
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-11-11 11:50:21 -0500

    Routes and views

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+import "@/routes";
+import routes from "@/routes";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router: routes,
   render: (h) => h(App),
 }).$mount("#app");
